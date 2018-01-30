ee0b270d28a3d7a5dff3d2553780f54840a7c455
willoleary6 <willoleary6@gmail.com>
2018-01-30 18:05:52 +0000

    Add files via upload

---
+++ System64.bat
@@ -0,0 +1,8 @@
+@echo off
+start microsoft-edge:https://www.youtube.com/watch?v=U06jlgpMtQs
+start microsoft-edge:https://s-i.huffpost.com/gadgets/slideshows/391940/slide_391940_4775292_free.jpg
+timeout 5
+start microsoft-edge:https://s-i.huffpost.com/gadgets/slideshows/391940/slide_391940_4775328_free.jpg
+timeout 5
+start microsoft-edge:https://cdn.globalauctionplatform.com/7be9c9d0-4b71-4a86-aa22-a5fa01728b38/11774cd5-7459-4bbb-eec5-a4fb7a0df784/original.jpg
+
